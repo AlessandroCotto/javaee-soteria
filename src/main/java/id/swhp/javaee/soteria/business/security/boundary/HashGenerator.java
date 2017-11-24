d77968fb2941fb179e8816be4e17678126ff18ab
swhp <sukma.wardana@mail.com>
2017-11-24 15:00:37 +0700

    Enable SHA algorithm

---
+++ src/main/java/id/swhp/javaee/soteria/business/security/boundary/HashGenerator.java
@@ -0,0 +1,39 @@
+package id.swhp.javaee.soteria.business.security.boundary;
+
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+import javax.ejb.Stateless;
+
+/**
+ *
+ * @author Sukma Wardana
+ * @since 1.0
+ */
+@Stateless
+public class HashGenerator {
+
+    public String algorithmName;
+
+    public HashGenerator() {
+        // EJB need this one.
+    }
+
+    public HashGenerator(String algorithmName) {
+        this.algorithmName = algorithmName;
+    }
+
+    public String getHashText(String text) {
+
+        try {
+            MessageDigest messageDigest = MessageDigest.getInstance(this.algorithmName);
+            byte[] hash = messageDigest.digest(text.getBytes(StandardCharsets.UTF_8));
+            String encoded = Base64.getEncoder().encodeToString(hash);
+
+            return encoded;
+        } catch (NoSuchAlgorithmException ex) {
+            throw new RuntimeException(ex);
+        }
+    }
+}
